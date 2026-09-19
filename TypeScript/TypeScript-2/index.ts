5aff5c2da2464d9957e3c1350953c196b396505e
piyush <piyushujgaokar544@gmail.com>
2026-09-19 23:51:12 +0530

    typescript data types,tuples,union,array

---
+++ TypeScript/TypeScript-2/index.ts
@@ -0,0 +1,39 @@
+// let a:string="dudu"
+// a=20
+// console.log(a)
+
+// let a:number=20
+// a="piyush"
+// console.log(a)
+
+
+
+// let arr:string[]=["piyush",23,44,undefined,true]
+// console.log(arr);
+
+// let arr:number[]=["piyush",23,44,undefined,true]
+// console.log(arr);
+
+
+
+// enum Role{
+//     ADMIN,//0
+//     SUP_ADMIN,//1
+//     USER//2
+// }
+
+// let role:Role=Role.SUP_ADMIN
+
+// console.log(role); //gives 1 because Role object gives index 
+
+
+
+// let arr:[number,number]=[10,20]
+
+// console.log(arr);
+
+// let arr:[number,number,string,undefined]=[10,20,"piyush",undefined]
+// console.log(arr);
+
+
+
