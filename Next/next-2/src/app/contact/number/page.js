f5ef1c1da06e8722d2a9d3c603861699603f01fe
piyush <piyushujgaokar544@gmail.com>
2026-09-21 22:53:38 +0530

    Understanding Routing

---
+++ Next/next-2/src/app/contact/number/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      This is number page
+    </div>
+  );
+}
+
+export default page;
---
+++ Next/next-2/src/app/layout.js
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        hello
+        {children}</body>
+    </html>
+  );
+}
