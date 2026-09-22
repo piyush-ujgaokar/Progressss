9390e6315a34eb43d201f8ed4903bfc38f4b01f2
piyush <piyushujgaokar544@gmail.com>
2026-09-22 17:24:18 +0530

    App Router Done

---
+++ Next/next-3/src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notfound = () => {
+  return (
+    <div>
+      this is my not found page bhadwe
+    </div>
+  );
+}
+
+export default notfound;
--- Next/next-3/src/app/page.js
+++ Next/next-3/src/app/page.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const page = () => {
+const page = async() => {
+
+  await new Promise((resolve,reject)=> setTimeout(resolve,3000))
+
   return (
     <div>
       This is home page
