58d61ce8c92052b4114ba6774f29ef37263bcbad
piyush <piyushujgaokar544@gmail.com>
2026-09-22 15:52:14 +0530

    Understand nested routing

--- Next/next-2/src/app/layout.js
+++ Next/next-2/src/app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        hello
+        <div>hello</div>
         {children}</body>
     </html>
   );
