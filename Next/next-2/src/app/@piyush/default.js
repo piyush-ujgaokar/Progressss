34e07fc291ef86165cc29e59ddca704855668f32
piyush <piyushujgaokar544@gmail.com>
2026-09-22 17:05:52 +0530

    Parallel Routing

---
+++ Next/next-2/src/app/@piyush/default.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      This is piyush page
+    </div>
+  );
+}
+
+export default page;
--- Next/next-2/src/app/layout.js
+++ Next/next-2/src/app/layout.js
@@ -1,10 +1,17 @@
 import "./globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children,team,piyush }) {
   return (
     <html lang="en">
-      <body>
-        {children}</body>
+      <body  className="w-screen h-screen flex">
+       <div className="w-[50%] bg-green-500  "> {children}</div>
+        <div className="w-[50%] bg-red-600 ">
+          {team}
+          
+            <div className="w-[20%] h-[50%] bg-blue-600  ">{piyush}</div>
+          
+          </div>
+        </body>
     </html>
   );
 }
