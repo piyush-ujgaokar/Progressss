1996ea217b7d4c428e84a47c20586e197e02accd
piyush <piyushujgaokar544@gmail.com>
2026-09-22 19:19:37 +0530

    navigation using useRouter()

--- Next/next-3/src/app/page.js
+++ Next/next-3/src/app/page.js
@@ -1,10 +1,21 @@
+'use client'
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = async() => {
-  
+
+  const router=useRouter()
+
   return (
     <div>
-      This is home page
+     <Link href={'/home'}><h3>Home</h3></Link>
+     <Link href={'/about'}><h3>About</h3></Link>
+     <Link href={'/contact'}><h3>Contact</h3></Link>
+
+     <button onClick={()=>router.push('/about')}>
+      Go to google
+     </button>
     </div>
   );
 }
